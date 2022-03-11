b6e1f8f079ae5b5258296b1b43aecab0238835aa
daenamcclintock <daena.mcclintock@gmail.com>
2022-03-11 15:03:33 -0500

    added comment

--- server.js
+++ server.js
@@ -4,12 +4,15 @@
 // this allows us to load our env variables
 require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
-const methodOverride = require('method-override')
-
-// No longer need this reference bc it lives in the fruits.js controller now
+// We no longer need this reference because it lives in the fruit controller now
 // const Fruit = require('./models/fruit')
+// now that we're using controllers as they should be used
+// we need to require our routers
 const FruitRouter = require('./controllers/fruit')
+const UserRouter = require('./controllers/user')
+const HomeRouter = require('./controllers/home')
+const CommentRouter = require('./controllers/comment')
+const middleware = require('./utils/middleware')
 
 ////////////////////////////////////////////
 // Create our express application object
@@ -19,24 +22,23 @@ const app = require('liquid-express-views')(express())
 ////////////////////////////////////////////
 // Middleware
 ////////////////////////////////////////////
-// this is for request logging
-app.use(morgan('tiny'))
-app.use(methodOverride('_method'))
-// parses urlencoded request bodies
-app.use(express.urlencoded({ extended: false }))
-// to serve files from public statically
-app.use(express.static('public'))
+middleware(app)
 
 ////////////////////////////////////////////
 // Routes
 ////////////////////////////////////////////
-
-// send all /fruits routes of the Fruit router
+// register our routes here
+// send all '/fruits' routes to the Fruit Router
 app.use('/fruits', FruitRouter)
+app.use('/comments', CommentRouter)
+app.use('/user', UserRouter)
+app.use('/', HomeRouter)
 
-app.get('/', (req, res) => {
-    res.send('your server is running, better go catch it')
-})
+// old home, now we're using homerouter
+// app.get('/', (req, res) => {
+//     res.send('your server is running, better go catch it')
+// })
+
 
 ////////////////////////////////////////////
 // Server Listener
